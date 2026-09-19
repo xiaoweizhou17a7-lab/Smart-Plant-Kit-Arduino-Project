8d24aaf8c11684204817a3f9942af6d62f3daf6b
xiao <xiaoweizhou17a7@gmail.com>
2026-09-19 22:24:29 +0100

    testing board

--- Sketch.ino
+++ Sketch.ino
@@ -1,9 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-
+Serial.begin(9600); // initializes the serial port with a baud rate of 9600
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+Serial.println("hello, world"); // prints a string to a serial port
+delay(1000); //delay of 1 second
 }
+
