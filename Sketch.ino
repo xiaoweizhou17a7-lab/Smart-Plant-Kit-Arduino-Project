84d3f45a682bb6014004aba3d5ed1db089806aef
xiao <xiaoweizhou17a7@gmail.com>
2026-09-20 18:55:23 +0100

    Initialise Screen

--- Sketch.ino
+++ Sketch.ino
@@ -1,9 +1,13 @@
-void setup() {
-Serial.begin(9600); // initializes the serial port with a baud rate of 9600
+#include <Arduino.h>
+#include <U8x8lib.h>
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+// U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE); // OLEDs without Reset of the Display
+void setup(void) {
+u8x8.begin();
+u8x8.setFlipMode(1);
 }
-
-void loop() {
-Serial.println("hello, world"); // prints a string to a serial port
-delay(1000); //delay of 1 second
+void loop(void) {
+u8x8.setFont(u8x8_font_chroma48medium8_r);
+u8x8.setCursor(0, 0);
+u8x8.print("Hello Grove!");
 }
-
